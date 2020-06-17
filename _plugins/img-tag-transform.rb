9cd7874aa67a02068bf0ea50950c29119c2048c8
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-06-17 22:19:40 +0200

    more types for responsive hook

--- _plugins/img-tag-transform.rb
+++ _plugins/img-tag-transform.rb
@@ -1,4 +1,4 @@
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
+Jekyll::Hooks.register :posts, :pre_render do |post, pages, lesende|
   docExt = post.extname.tr('.', '')
   post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3 %}')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
