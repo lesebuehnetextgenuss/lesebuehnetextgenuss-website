c205ae973e314e9648440d4cf29c3b98a3171409
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-06-18 18:23:18 +0200

    fix galerie

--- _plugins/img-tag-transform.rb
+++ _plugins/img-tag-transform.rb
@@ -1,16 +1,10 @@
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
+Jekyll::Hooks.register :posts :lesende, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
   post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3 %}')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
 end
-Jekyll::Hooks.register :lesende, :pre_render do |post, payload|
-  docExt = post.extname.tr('.', '')
-  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3 %}')
-  post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
-end
-
 Jekyll::Hooks.register :galerie, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
-  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '<a href="\2">{% responsive_image path: \2 \3 %}</a>')
+  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '[{% responsive_image path: \2 \3 %}](\2)')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
 end
