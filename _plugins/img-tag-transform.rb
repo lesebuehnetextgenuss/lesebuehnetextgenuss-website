deba5792b0eb5089da05c682005be7652e3939ab
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-06-18 18:08:50 +0200

    galerie image

--- _plugins/img-tag-transform.rb
+++ _plugins/img-tag-transform.rb
@@ -1,5 +1,16 @@
-Jekyll::Hooks.register :posts, :pre_render do |post, pages, lesende|
+Jekyll::Hooks.register :posts, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
   post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3 %}')
+  post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
+end
+Jekyll::Hooks.register :lesende, :pre_render do |post, payload|
+  docExt = post.extname.tr('.', '')
+  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3 %}')
+  post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
+end
+
+Jekyll::Hooks.register :galerie, :pre_render do |post, payload|
+  docExt = post.extname.tr('.', '')
+  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% galerie_image path: \2 \3 %}')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
 end
