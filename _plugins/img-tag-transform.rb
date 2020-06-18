72da81c970cb86ddb92ed14aa8b443c60a836f4b
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-06-18 19:14:10 +0200

    add class to link

--- _plugins/img-tag-transform.rb
+++ _plugins/img-tag-transform.rb
@@ -11,6 +11,6 @@ end
 
 Jekyll::Hooks.register :galerie, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
-  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '[{% responsive_image path: \2 \3 %}](\2){.glightbox}')
+  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '[{% responsive_image path: \2 \3 %}](\2){:.glightbox}')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
 end
