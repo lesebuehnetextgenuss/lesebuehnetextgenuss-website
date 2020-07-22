9221f83a1f0644767e1e55f6a4f500cd59527aec
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-07-22 19:03:37 +0200

    better image responsiveness

--- _plugins/img-tag-transform.rb
+++ _plugins/img-tag-transform.rb
@@ -15,6 +15,6 @@ Jekyll::Hooks.register :lesende, :pre_render do |post, payload|
 end
 Jekyll::Hooks.register :galerie, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
-  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '[{% responsive_image path: \2 \3 %}](\2){:lightbox}')
+  post.content.gsub!(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 \3  template: _includes/responsive_image_gallery.html %}')
   post.content.gsub! 'path: /', 'path: ' #you can probably optimise this a bit
 end
